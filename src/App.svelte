0fee7bee9e553820e0ecd70fffbd6c4370f7080e
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-12-17 05:41:34 -0500

    [U] Update link

--- src/App.svelte
+++ src/App.svelte
@@ -284,7 +284,7 @@
   <div class="heading">Azalea's Colorful Slither Link</div>
   <div class="sub-heading">
     <img src={viteLogo} alt="Logo"/>
-    <span><a href="https://github.com/hykilpikonna/slither-link">GitHub</a> | <a href="https://aza.moe">Blog</a></span>
+    <span><a href="https://github.com/hykilpikonna/colorful-link">GitHub</a> | <a href="https://aza.moe">Blog</a></span>
     <img src={svelteLogo} alt="Logo"/>
   </div>
   <div class="rules">
@@ -391,7 +391,7 @@
       <p>Great job on solving your first puzzle 🧩 </p>
       <p>This project is just starting out, I hope you enjoyed it!</p>
       <p>If you want to design your own puzzle and share with the world, you can go to <a href="/?edit=1">Edit Mode</a> and start drawing!</p>
-      <p>You can find more about the creation process in the documentation on the <a href="https://github.com/hykilpikonna/slither-link">GitHub Repo</a>.</p>
+      <p>You can find more about the creation process in the documentation on the <a href="https://github.com/hykilpikonna/colorful-link">GitHub Repo</a>.</p>
       <p>If you want to chat, you can reach me on telegram <a href="https://t.me/hykilpikonna">@hykilpikonna</a>.</p>
       <p>Thanks for playing! 🤗</p>
 
